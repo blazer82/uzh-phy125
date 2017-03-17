008b62b7be7a2c744184c2aa2ede60fba4618884
Raphael Stäbler <staebler@deluxx.ch>
2017-03-17 22:43:14 +0100

    Minor improvements to comet halley exercise

--- exercise4b.py
+++ exercise4b.py
@@ -17,11 +17,11 @@ def kepler_prime(E, e):
     return 1 - e * cos(E)
 
 # Define coordinate function
-def coordinate(e, E, alpha):
-    return (alpha * (cos(E) - e), alpha * (sqrt(1 - e**2) * sin(E)))
+def coordinate(e, E, a):
+    return (a * (cos(E) - e), a * (sqrt(1 - e**2) * sin(E)))
 
 # Define function to find root of functions
-def find_root(func, func_prime, x=0):
+def find_root(func, func_prime, x=1):
     iterations = []
     while not x in iterations:
         iterations.append(x)
@@ -31,8 +31,8 @@ def find_root(func, func_prime, x=0):
 
 if __name__ == '__main__':
 
-    alpha = 17.8
-    e = 0.967
+    a = 17.8 # Semi-major axis
+    e = 0.967 # Eccentricity
 
     nbr_of_steps = 100
 
@@ -40,9 +40,7 @@ if __name__ == '__main__':
 
     x = []
     y = []
-    for i in range(nbr_of_steps):
-        M = M_space[i]
-
+    for M in M_space:
         def local_kepler(E):
             return kepler(E, e, M)
 
@@ -53,14 +51,16 @@ if __name__ == '__main__':
         E = find_root(local_kepler, local_kepler_prime)
 
         # Calculate coordinates
-        (x_i, y_i) = coordinate(e, E, alpha)
+        (x_i, y_i) = coordinate(e, E, a)
         x.append(x_i)
         y.append(y_i)
 
     # Plot results
     plt.title("Comet Halley's Orbit")
+    plt.xlabel("AU")
+    plt.ylabel("AU")
     plt.plot(x, y, '.')
-    plt.plot([0], [0], '.') # Plot the sun (0,0) for reference
+    plt.plot([0], [0], '.') # Plot the sun (0,0) (approximately) for reference
     plt.axis('equal')
     plt.savefig('comet_halley.pdf')
     plt.show()
