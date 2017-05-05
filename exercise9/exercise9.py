64f6fdeaa03859198ddc76d82f98bd8f34d97632
Raphael Stäbler <staebler@deluxx.ch>
2017-05-05 14:00:08 +0200

    Added comments to exercise 9

--- exercise9/exercise9.py
+++ exercise9/exercise9.py
@@ -6,6 +6,21 @@ import numpy as np
 
 
 def rectangle_method(a, b, func, N=10):
+    """
+    Implementation of the rectangle method to integrate
+
+    Arguments
+    ---------
+    a: float, Lower boundary
+    b: floar, Upper boundary
+    func: function, Function to integrate
+    N: int, amount of slices to use
+
+    Return
+    ------
+    float, Integration result
+    """
+
     # Slice width
     h = (b - a) / N
 
@@ -21,14 +36,16 @@ def rectangle_method(a, b, func, N=10):
 
 if __name__ == '__main__':
 
-    a = 0
-    b = 2
-    f = np.cos
-    F = np.sin
-    result = F(b) - F(a)
+    a = 0  # Lower boundary
+    b = 2  # Upper boundary
+    f = np.cos  # Function to integrate
+    F = np.sin  # Antiderivative
+    result = F(b) - F(a)  # Correct result
 
+    # Amount of slices to use
     slices = 10
 
+    # Rectangle method
     rect_result = rectangle_method(a, b, f, slices)
     rect_error = rect_result - result
     print("\nRectangle method ({slices} slices): {result} (error {error})".format(result=rect_result, error=rect_error, slices=slices))
